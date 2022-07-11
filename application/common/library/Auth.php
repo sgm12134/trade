213065d843beeec3a4f272057709856e2541402f
sgm1314 <songg8805@gmail.com>
2022-07-11 20:41:03 +0800

    ''

--- application/common/library/Auth.php
+++ application/common/library/Auth.php
@@ -26,7 +26,7 @@ class Auth
     //默认配置
     protected $config = [];
     protected $options = [];
-    protected $allowFields = ['id', 'username', 'money', 'mobile', 'avatar', 'pay_password'];
+    protected $allowFields = ['id', 'username', 'money', 'mobile', 'avatar', 'pay_password','invite_code'];
 
     public function __construct($options = [])
     {
