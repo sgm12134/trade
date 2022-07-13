28135099270584185756b94935d709e3183e51bd
sgm1314 <songg8805@gmail.com>
2022-07-13 09:55:33 +0800

    ''

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -335,7 +335,7 @@ class Index extends Api
         foreach ($data as $k=>$v){
             $v->time_str=date('Y-m-d H:i:s',$v->time);
             $v->submit_time_str=date('Y-m-d H:i:s',$v->submit_time);
-            $v->after_money=Db::name('order')->where('order_no',$v->order_no)->value('after');
+            $v->after_money=Db::name('user_money_log')->where('order_no',$v->order_no)->value('after');
             if($v->state ==1){
                 $v->state_str='待审核';
             }else if($v->state ==2){
